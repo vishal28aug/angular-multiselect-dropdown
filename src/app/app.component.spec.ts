9b7ff0d0e4c96b20a3aa6f6e55ecb855c6de9a84
vishal28aug <vishalsharma25sept@gmail.com>
2020-07-03 18:48:49 +0530

    Changed Project name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'demo'`, () => {
+  it(`should have as title 'MultiSelect-Dropdown'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('demo');
+    expect(app.title).toEqual('MultiSelect-Dropdown');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('demo app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('MultiSelect-Dropdown app is running!');
   });
 });
